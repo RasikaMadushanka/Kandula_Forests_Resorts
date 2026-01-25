3df08d487309ebe8dab79ab5338bb0d55234b724
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-25 22:14:02 +0530

    home/safari section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../../component/nav-bar/nav-bar.component";
 import { ImageFrameComponent } from "../../component/image-frame/image-frame.component";
 import { OverviewComponent } from "../../component/overview/overview.component";
+import { HomeRoomsComponent } from '../../component/home-rooms/home-rooms.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, ImageFrameComponent, OverviewComponent],
+  imports: [NavBarComponent, ImageFrameComponent, OverviewComponent,HomeRoomsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
