a5cc62ac1348c79d9069259382d6d22bf06d4cac
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-25 19:18:07 +0530

    home/room section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../component/nav-bar/nav-bar.component";
 import { ImageFrameComponent } from "../../component/image-frame/image-frame.component";
+import { OverviewComponent } from "../../component/overview/overview.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, ImageFrameComponent],
+  imports: [NavBarComponent, ImageFrameComponent, OverviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
