2b5a0968df0929f558821f774a22cdee75366823
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-31 21:58:08 +0530

    make contactus page - whatsapp form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { RoomsComponent } from './pages/rooms/rooms.component';
 import { RoomDetailsailsComponent } from './pages/room-detailsails/room-detailsails.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 export const routes: Routes = [
      { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -21,6 +22,10 @@ export const routes: Routes = [
   {
     path:'about-us',
     component:AboutUsComponent
+  },
+   {
+    path:'contact-us',
+    component:ContactUsComponent
   }
     
 ];
--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../component/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
