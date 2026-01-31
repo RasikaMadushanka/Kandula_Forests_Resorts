a54f42c732ec1d9124d959163e30063ae8f48c0c
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-31 21:42:44 +0530

    make contactus page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RoomsComponent } from './pages/rooms/rooms.component';
 import { RoomDetailsailsComponent } from './pages/room-detailsails/room-detailsails.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
      { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -16,6 +17,10 @@ export const routes: Routes = [
   {
     path: 'rooms-details/:id',
     component: RoomDetailsailsComponent
+  },
+  {
+    path:'about-us',
+    component:AboutUsComponent
   }
     
 ];
