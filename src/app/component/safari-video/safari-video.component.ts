99dc7ca8d790871cc6e723c01eba8e5842e571f2
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-02-01 13:59:29 +0530

    make componet add video

---
+++ src/app/component/safari-video/safari-video.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-safari-video',
+  standalone: true,
+  imports: [],
+  templateUrl: './safari-video.component.html',
+  styleUrl: './safari-video.component.css'
+})
+export class SafariVideoComponent {
+
+}
